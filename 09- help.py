eae95190aa91ff09fa4cacd1fbad0894337cbf35
Camilo Castillo <npgcamilo@gmail.com>
2019-12-21 12:53:11 -0500

    help

---
+++ 09- help.py
@@ -0,0 +1,9 @@
+# para documentar las funciones podemos utilizar el formato de """ """
+
+def my_function():
+    """ documentación de la función """
+    pass
+
+# con la funcion global help podemos ver la documentacion de cualquier funcion que se pase 
+# por parametro
+help(my_function)    
