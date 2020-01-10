99f9bc82a14d8812ccce4a294fc39b57ba6edebc
Camilo Castillo <npgcamilo@gmail.com>
2020-01-10 10:04:12 -0500

    slides

---
+++ 15-slides.py
@@ -0,0 +1,15 @@
+# Los slides son una forma de tomar porciones de un string
+
+my_name = "electroencefalografista"
+
+# Primera letra
+print(my_name[0])
+
+# Ultima letra
+print(my_name[-1])
+
+# De la primera a la cuarta no incluyente
+print(my_name[0:3])
+
+# Del inicio hasta el final del string saltando 2 caracteres
+print(my_name[::2])
